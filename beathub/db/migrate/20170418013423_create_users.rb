78bae9baefd0320a3afeefd4bc2985fbdb9c7c52
Brandon Thaler <bthaler@bellsouth.net>
2017-04-17 21:38:44 -0400

    add User model and controller and Sessions controller

--- beathub/db/migrate/20170418013423_create_users.rb
+++ beathub/db/migrate/20170418013423_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :email
-      t.string :password_digest
-      t.string :session_token
+      t.string :username, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
       t.string :image_url
 
       t.timestamps null: false
