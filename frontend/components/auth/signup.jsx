4182d7105e6778c2958a6c8976d0a7ff91487180
App Academy Student <dt1041@nyu.edu>
2017-04-18 23:44:52 -0400

    add error display on signup

--- frontend/components/auth/signup.jsx
+++ frontend/components/auth/signup.jsx
@@ -10,6 +10,7 @@ class SignUp extends React.Component{
     this.handleInput = this.handleInput.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.parseErrors = this.parseErrors.bind(this);
+    this.resetErrors = this.resetErrors.bind(this);
   }
 
   handleInput(field){
@@ -21,37 +22,51 @@ class SignUp extends React.Component{
 
   handleSubmit(e){
     e.preventDefault();
+    this.resetErrors();
     this.props.signup(this.state).then(() => this.props.router.push('/'), alert("something went wrong"));
+  }
+
+  resetErrors(){
+    let elements = document.getElementsByClassName("show");
+    if (elements.length > 0){
+      for (let i = 0; i < elements.length; i++){
+        elements[i].setAttribute("class", "form-errors");
+      }
+    }
   }
 
   parseErrors(errors){
     let errorHash = {};
     let first_word;
+    let element;
     errors.forEach((error) => {
       first_word = error.split(" ")[0];
       errorHash[first_word] = error;
+      element = document.getElementById(`${first_word}`);
+      if (element){
+        element.setAttribute("class", "show");
+      }
     });
     return errorHash;
   }
 
   render(){
-
     const errors = this.props.errors;
     let collectErrors = errors ? this.parseErrors(errors) : "";
     const usernameErrors = collectErrors.Username ? collectErrors.Username : "";
     const emailErrors = collectErrors.Email ? collectErrors.Email : "";
     const passwordErrors = collectErrors.Password ? collectErrors.Password : "";
-    debugger
+
     return (
       <div className="signup-form-page">
         <div className="signup-container">
           <h3>Sign up with your email address </h3>
           <form className="signup-form">
-            <input type="text" value={this.state.username} placeholder="e.g.marshmallow" onChange={this.handleInput('username')}/><div className="form-errors" id="usernameErrors">{usernameErrors}</div>
+            <input type="text" value={this.state.username} placeholder="e.g.marshmallow" onChange={this.handleInput('username')}/><div className="form-errors" id="Username">{usernameErrors}</div>
             <br/>
-            <input type="email" value={this.state.email} placeholder="e.g.marshmallow@email.com" onChange={this.handleInput('email')}/><div className="form-errors" id="emailErrors">{emailErrors}</div>
+            <input type="email" value={this.state.email} placeholder="e.g.marshmallow@email.com" onChange={this.handleInput('email')}/><div className="form-errors" id="Email">{emailErrors}</div>
             <br/>
-            <input type="password" value={this.state.password} placeholder="Choose a password" onChange={this.handleInput('password')}/><div className="form-errors" id="passwordErrors">{passwordErrors}</div>
+            <input type="password" value={this.state.password} placeholder="Choose a password" onChange={this.handleInput('password')}/><div className="form-errors" id="Password">{passwordErrors}</div>
             <br/>
             <button onClick={this.handleSubmit}>SIGN UP</button>
           </form>
