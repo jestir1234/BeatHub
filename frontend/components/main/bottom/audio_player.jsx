d12a8fc869da3c39e7276fe94393c71836958a40
jestir1234@gmail.com <jestir1234@gmail.com>
2017-04-22 07:15:30 -0400

    begin audio player

--- frontend/components/main/bottom/audio_player.jsx
+++ frontend/components/main/bottom/audio_player.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SoundComponent from './sound_component';
+import { Line, Circle } from 'rc-progress';
 
 class AudioPlayer extends React.Component{
   constructor(props){
@@ -15,14 +16,36 @@ class AudioPlayer extends React.Component{
   }
 
   render(){
-    
+
     const currentSong = this.props.currentSong ? this.props.currentSong : null;
     const playSong = currentSong ? this.playSong(this.props) : null;
 
     return(
       <div className="bottom-content">
+
         <h1>{currentSong ? `${currentSong.name} is currently playing...` : "No songs playing"}</h1>
         {playSong}
+
+        <div className="audio-display">
+            <div className="play-controls">
+              <button></button>
+              <button></button>
+              <button></button>
+              <button></button>
+              <button></button>
+            </div>
+            <div className="status-bar">
+              <p>time passed</p>
+              <Line percent="65" strokeWidth="1" strokeColor="green" trailWidth="1"/>
+              <p>time left</p>
+            </div>
+
+        </div>
+
+
+        <div className="volume-control">
+          <p>volume controls here</p>
+        </div>
       </div>
     );
   }
