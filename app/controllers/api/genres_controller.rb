7249784e85c7a6fcd5402c6a8370e90cc13ee7df
MATT <jestir1234@gmail.com>
2017-06-15 16:35:32 -0400

    add

--- app/controllers/api/genres_controller.rb
+++ app/controllers/api/genres_controller.rb
@@ -1,23 +1,23 @@
 class Api::GenresController < ApplicationController
 
   def index
-    synth_pop_songs = Song.where(genre: "Synth-pop")
-    dream_pop_songs = Song.where(genre: "Dream-pop")
-    shoe_gaze_songs = Song.where(genre: "Shoe-gaze")
-    soul_songs = Song.where(genre: "soul")
-    indie_rock_songs = Song.where(genre: "Indie-rock")
-    j_rock_songs = Song.where(genre: "J-Rock")
-    punk_rock_songs = Song.where(genre: "Punk-Rock")
+    synth_pop = Artist.where(genre: "Synth-pop")
+    dream_pop = Artist.where(genre: "Dream-pop")
+    shoe_gaze = Artist.where(genre: "Shoe-gaze")
+    soul = Artist.where(genre: "soul")
+    indie_rock = Artist.where(genre: "Indie-rock")
+    j_rock = Artist.where(genre: "J-Rock")
+    punk_rock = Artist.where(genre: "Punk-Rock")
 
 
     @genres = {
-      synth_pop: synth_pop_songs,
-      dream_pop: dream_pop_songs,
-      shoe_gaze: shoe_gaze_songs,
-      soul: soul_songs,
-      indie_rock: indie_rock_songs,
-      j_rock: j_rock_songs,
-      punk_rock: punk_rock_songs,
+      synth_pop: synth_pop
+      dream_pop: dream_pop
+      shoe_gaze: shoe_gaze
+      soul: soul
+      indie_rock: indie_rock
+      j_rock: j_rock
+      punk_rock: punk_rock
     }
 
     render 'api/genres/index'
--- app/views/api/genres/index.json.jbuilder
+++ app/views/api/genres/index.json.jbuilder
@@ -1,10 +1,1 @@
-genres = @genres
-json.genres do
-  @genres.keys.each do |genre|
-    songs = genres[genre]
-    json.set! genre, songs.each do |song|
-      json.partial! "api/songs/songs", song: song
-    end
-  end
-end
-# json.genres @genres
+json.genre @genres
